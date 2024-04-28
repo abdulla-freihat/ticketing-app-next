d8b3f872feb633540ecbffa967c4c17acffda6a2
abdulla freihat <abdulla_freihat@hotmail.com>
2024-04-27 23:48:24 -0400

    add ticket card ui component  and its components and install mongo db and mongoose

---
+++ app/components/ProgressDisplay.js
@@ -0,0 +1,10 @@
+
+const ProgressDisplay = () => {
+  return (
+    <div className="bg-gray-200 w-full h-2.5 rounded-full ">
+        <div className="bg-blue-600 w-[75%] h-2.5 rounded-full"></div>
+    </div>
+  )
+}
+
+export default ProgressDisplay
---
+++ app/components/StatusDisplay.js
@@ -0,0 +1,10 @@
+
+const StatusDisplay = () => {
+  return (
+   <span className="inline-block cursor-pointer rounded-full px-2 py-1 font-semibols text-gray-700 text-xs  bg-green-200">
+     done
+   </span>
+  )
+}
+
+export default StatusDisplay
